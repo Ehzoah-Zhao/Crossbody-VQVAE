22937429340b2c5939e9f3b8cfe95fa676b9c5a3
Ehzoah <zhaohaozhe86@gmail.com>
2026-05-28 19:29:49 +0800

    v4删除无用文件

--- _gen.py
+++
@@ -1,8 +0,0 @@
-﻿import os
-os.makedirs(r"F:\VQVAE\utils", exist_ok=True)
-os.makedirs(r"F:\VQVAE\models", exist_ok=True)
-
-# Write a simple test
-with open(r"F:\VQVAE\utils\physics_losses.py", "w", encoding="utf-8") as f:
-    f.write("# test\nprint('hello')\n")
-print("OK")
